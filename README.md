4502b76ce0ddb29575d4d27b5c56a98e2f4196dd
Andrew Martin <mr.a.martin@gmail.com>
2015-08-18 11:49:22 +0100

    Added unicode support. Modified README.
    	modified:   README.md
    	modified:   todo_py.py

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 ## todo_py - A minimal CLI JSON-based task list.
+
+### Instructions
+
+Clone the repo to wherever you put applications. I put it in my home directory.
+
+Create a task list file, it can be anywhere, and called anything, but it should contain simply a pair of square brackets.
+
+###### my_tasks.json
+```
+[]
+```
+
+Either (i) put "todo.py" on your path, or (ii) use its relative or absolute path. E.g. ~/my_progs/todo_py/todo.py
--- todo_py.py
+++ todo_py.py
@@ -13,25 +13,25 @@ def load_tasks(task_list_file_name):
 
 	task_list = json.loads(task_list_data)
 	for task in task_list:
-		if not "id" in task:
-			task["id"] = str(uuid.uuid4())
-		if not "note" in task:
-			task["note"] = ""
+		if not u"id" in task:
+			task[u"id"] = str(uuid.uuid4())
+		if not u"note" in task:
+			task[u"note"] = u""
 	return task_list
 
 complete_statuses = [
-	'done',
-	'abandoned'
+	u"done",
+	u"abandoned"
 ]
 
 urgent_statuses = [
-	"[Bloody Urgent]",
-	"[Urgent]"
+	u"[Bloody Urgent]",
+	u"[Urgent]"
 ]
 
-default_status = "not done"
+default_status = u"not done"
 
-dt_format_str = "%d/%m/%Y - %H:%M:%S"
+dt_format_str = u"%d/%m/%Y - %H:%M:%S"
 
 def get_now_datetime():
 	return datetime.datetime.now()
@@ -45,133 +45,133 @@ def datetime_to_dt_str(dt):
 def dt_str_to_datetime(dt_str):
 	return datetime.datetime.strptime(dt_str,dt_format_str)
 
-task_template = (''
-'{{\n'
-'	"name": "",\n'
-'	"detail": [\n'
-'		""\n'
-'	],\n'
-'	"status": "not done",\n'
-'	"note": "",\n'
-'	"hide": "{now}"\n'
-'}}\n').format(
+task_template = (u""
+u"{{\n"
+u"	\"name\": \"\",\n"
+u"	\"detail\": [\n"
+u"		\"\"\n"
+u"	],\n"
+u"	\"status\": \"not done\",\n"
+u"	\"note\": \"\",\n"
+u"	\"hide\": \"{now}\"\n"
+u"}}\n").format(
 	now = datetime_to_dt_str(get_now_date()))
 
 def get_field(task,field):
 	if field in task:
 		return task[field]
 	else:
-		raise KeyError('Task {task_id} missing a "{field}" field'.format(
-			task_id=task["id"][:8],
+		raise KeyError(u"Task {task_id} missing a \"{field}\" field".format(
+			task_id=task[u"id"][:8],
 			field=field))
 
 def task_hide(task):
 	# Hide task returns true if the task has a hide field, and the hide field
 	# encodes a time later (greater) than now.
 	return (
-		'hide' in task
-		and dt_str_to_datetime(task['hide']) > get_now_datetime())
+		u"hide" in task
+		and dt_str_to_datetime(task[u"hide"]) > get_now_datetime())
 
 def task_incomplete(task):
 	return (
-		not 'status' in task 
-		or task['status'] not in complete_statuses)
+		not u"status" in task
+		or task[u"status"] not in complete_statuses)
 
 def task_urgent(task):
 	return (
-		not 'status' in task 
-		or any((x in task['status'] for x in urgent_statuses)))
+		not u"status" in task
+		or any((x in task[u"status"] for x in urgent_statuses)))
 
 def quick_list(task_list):
 	out_lines = []
 	for task in task_list:
 		if task_incomplete(task) and not task_hide(task):
-			task_id = task["id"]
-			task_info = [get_field(task,'name')]
-			#name = get_field(task,'name')
-			status = get_field(task,'status')
+			task_id = task[u"id"]
+			task_info = [get_field(task,u"name")]
+			#name = get_field(task,u"name")
+			status = get_field(task,u"status")
 			if not status == default_status:
 				task_info.append(status)
 
 			out_lines.append(
-				"{task_id}: {task_info}".format(
+				u"{task_id}: {task_info}".format(
 					task_id=task_id[:8],
-					task_info=" - ".join(task_info))
+					task_info=u" - ".join(task_info))
 			)
-	return "\n".join(out_lines)
+	return u"\n".join(out_lines)
 
 def verbose_task(task):
 	out_lines = []
-	name = get_field(task,'name')
-	task_id = task["id"]
-	out_lines.append("{name}".format(
+	name = get_field(task,u"name")
+	task_id = task[u"id"]
+	out_lines.append(u"{name}".format(
 		task_id=task_id[:8],
 		name=name))
-	detail = get_field(task,'detail')
+	detail = get_field(task,u"detail")
 	for detail_line in detail:
-		out_lines.append("\t{detail_line}".format(
+		out_lines.append(u"\t{detail_line}".format(
 			detail_line=detail_line))
 	other_fields = [x for x in task.keys() if x not in [
-		'name',
-		'detail',
-		'status']]
+		u"name",
+		u"detail",
+		u"status"]]
 	other_fields.sort()
 	for field_name in other_fields:
-		out_lines.append("\t{field_name}: {field_value}".format(
+		out_lines.append(u"\t{field_name}: {field_value}".format(
 			field_name=field_name,
 			field_value=task[field_name]))
-	status = "\tStatus: {status}\n".format(
-		status=get_field(task,'status'))
+	status = u"\tStatus: {status}\n".format(
+		status=get_field(task,u"status"))
 	out_lines.append(status)
 	return out_lines
 
 # def ordered_json_task(task):
 # 	out_lines = []
-# 	top_fields = ["id","name","detail","status","note"]
+# 	top_fields = [u"id",u"name",u"detail",u"status",u"note"]
 # 	for field in top_fields:
 # 		if field in task:
-# 			out_lines.append("\"{field}\": \"{value}\"".format(
+# 			out_lines.append(u"\"{field}\u": \"{value}\u"".format(
 # 				field=field,
 # 				value=task[field]))
 # 	other_fields = [x for x in task.keys() if x not in top_fields]
 # 	for field in other_fields:
-# 		out_lines.append("\"{field}\": \"{value}\"".format(
+# 		out_lines.append(u"\"{field}\": \"{value}\"u".format(
 # 			field=field,
 # 			value=task[field]))
 #
-# 	return "{\n" + "\n".join(out_lines) + "\n}"
+# 	return u"{\n" + u"\n".join(out_lines) + u"\n}"
 
 
 def get_task_by_id(task_list,task_id):
 	for task in task_list:
-		if task["id"].startswith(task_id):
+		if task[u"id"].startswith(task_id):
 			return task
 	return None
 
 def get_task_index_by_id(task_list,task_id):
 	for num,task in enumerate(task_list):
-		if task["id"].startswith(task_id):
+		if task[u"id"].startswith(task_id):
 			return num
 	return None
 
 def verbose_task_from_list(task_list,args):
-	if 'task_id' in args:
-		task_id = args["task_id"]
+	if u"task_id" in args:
+		task_id = args[u"task_id"]
 		task = get_task_by_id(task_list,task_id)
 		if task:
-			return "\n".join(verbose_task(task))
+			return u"\n".join(verbose_task(task))
 	return None
 
 def get_empty_task():
-	#with open(os.path.join('templates','task_template.json'),'r') as file:
+	#with open(os.path.join(u"templates",u"task_template.json"),u"r") as file:
 	#	task_template = file.read()
 	#empty_task = json.loads(task_template[:-2]) # remove trailing comma
 	empty_task = {
-		"name":"",
-		"detail":[""],
-		"status":"not done",
-		"note":"",
-		"id":str(uuid.uuid4())
+		u"name":u"",
+		u"detail":[u""],
+		u"status":u"not done",
+		u"note":u"",
+		u"id":str(uuid.uuid4())
 	}
 	return empty_task
 
@@ -181,74 +181,78 @@ def editor_task(original_text):
 	while not valid_json:
 		edited_task_text = textedit.read_from_editor(
 			text_to_edit,
-			suffix=".json")
+			suffix=u".json")
 		try:
 			edited_task = json.loads(edited_task_text)
 			valid_json = True
 		except ValueError:
 			valid_json = False
-			text_to_edit = original_text + "\n==========\n" + edited_task_text
+			text_to_edit = original_text + u"\n==========\n" + edited_task_text
 	return edited_task
 
 
 def edit_task(task_list,args):
-	task_to_edit_index = get_task_index_by_id(task_list,args["task_id"])
-	task_list[task_to_edit_index] = editor_task(
-		json.dumps(
-			task_list[task_to_edit_index],
-			sort_keys=True,
-			indent=2,
-			separators=(',', ': ')))
+	task_to_edit_index = get_task_index_by_id(task_list,args[u"task_id"])
+	if task_to_edit_index is None:
+		return u"Not a recognised task \"{id}\"".format(id=args[u"task_id"])
+	else:
+		task_to_edit = task_list[task_to_edit_index]
+		task_list[task_to_edit_index] = editor_task(
+			json.dumps(
+				task_to_edit,
+				sort_keys=True,
+				indent=2,
+				separators=(u",", u": ")))
 
-	#task_list[task_to_edit_index]["note"] += " edited"
-	save_task_list(task_list,args["path"])
-	return "Updated task:\n{task}".format(
-		task="\n".join(verbose_task(task_list[task_to_edit_index])))
+		#task_list[task_to_edit_index][u"note"] += u" edited"
+		save_task_list(task_list,args[u"path"])
+		return u"Updated task:\n{task}".format(
+			task=u"\n".join(verbose_task(task_list[task_to_edit_index])))
 
 
 def save_task_list(task_list,path):
-	with open(path,"w") as file:
+	with open(path,u"w") as file:
 		file.write(json.dumps(
 			task_list,
 			sort_keys=True,
 			indent=2,
-			separators=(',', ': ')))
+			separators=(u",", u": ")))
 
 def new_task(task_list,args):
 
 	new_task = editor_task(task_template)
 
 	#new_task_text = textedit.read_from_editor(
-	#	task_template,suffix=".json")
+	#	task_template,suffix=u".json")
 	#new_task = json.loads(new_task_text)
-	new_task["id"] = str(uuid.uuid4())
+	new_task[u"id"] = str(uuid.uuid4())
 	task_list.append(new_task)
-	save_task_list(task_list,args["path"])
-	return "New task:\n{task}".format(
-		task="\n".join(verbose_task(new_task)))
+	save_task_list(task_list,args[u"path"])
+	return u"New task:\n{task}".format(
+		task=u"\n".join(verbose_task(new_task)))
 
 def verbose_list(task_list,args):
 	out_lines = []
 	for task in task_list:
 		if task_incomplete(task):
 			out_lines.extend(verbose_task(task))
-	return "\n".join(out_lines)
+	return u"\n".join(out_lines)
 
 def urgent_list(task_list,args):
 	out_lines = []
 	for task in task_list:
 		if task_urgent(task):
 			out_lines.extend(verbose_task(task))
-	return "\n".join(out_lines)
+	return u"\n".join(out_lines)
 	
 
 def get_function_for_command(first_arg):
 	valid_args = {
-		"e":edit_task, # edit
-		"n":new_task, # new
-		"s":verbose_task_from_list, # show
-		"u":urgent_list, # urgent
-		"v":verbose_list, # verbose
+		u"e":edit_task, # edit
+		u"n":new_task, # new
+		u"s":verbose_task_from_list, # show
+		u"u":urgent_list, # urgent
+		u"v":verbose_list, # verbose
 	}
 
 	if first_arg in valid_args:
@@ -258,7 +262,7 @@ def get_function_for_command(first_arg):
 
 def args_to_dict(args):
 	args_dict = {}
-	arg_list = ['script','path','command','task_id']
+	arg_list = [u"script",u"path",u"command",u"task_id"]
 	for num,arg in enumerate(arg_list):
 		if len(args) > num:
 			args_dict[arg] = args[num]
